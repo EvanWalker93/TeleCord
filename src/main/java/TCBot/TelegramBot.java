7928ff9f19409a7dfb7250c28a2a704d936d37c0
Evan Walker <evanwalker93@gmail.com>
2017-09-14 16:32:02 -0400

    Working on image handling

--- src/main/java/TCBot/TelegramBot.java
+++ src/main/java/TCBot/TelegramBot.java
@@ -1,25 +1,34 @@
 package main.java.TCBot;
 
-import org.telegram.telegrambots.api.methods.GetFile;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.File;
-import org.telegram.telegrambots.api.objects.PhotoSize;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
     private TelegramMessageListener listener;
 
 
-    public interface TelegramMessageListener {
-        void onTelegramMessageReceived(SendMessage message, String channel, String author) throws TelegramApiException, IOException;
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("Telegram update received...");
+
+        SendMessage newMessage = new SendMessage().setChatId(update.getMessage().getChatId()).setText(update.getMessage().getText());
+        String channel = update.getMessage().getChatId().toString();
+        String username =  update.getMessage().getFrom().getUserName();
+        List<File> attachment = (List<File>) update.getMessage().getDocument();
+
+        try {
+            listener.onTelegramMessageReceived(newMessage, channel, username, attachment);
+            System.out.println("Sending message to TeleCordBot");
+        } catch (TelegramApiException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     TelegramBot(TelegramMessageListener listener) {
@@ -38,44 +47,11 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        System.out.println("Telegram update received...");
-
-        SendMessage newMessage = new SendMessage().setChatId(update.getMessage().getChatId()).setText(update.getMessage().getText());
-        String channel = update.getMessage().getChatId().toString();
-        String username =  update.getMessage().getFrom().getUserName();
-
-        if (update.getMessage().hasPhoto()) {
-
-        }
-
-
-        try {
-            listener.onTelegramMessageReceived(newMessage, channel, username);
-            System.out.println("Sending message to TeleCordBot");
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public interface TelegramMessageListener {
+        void onTelegramMessageReceived(SendMessage message, String channel, String author, List<File> attachment) throws TelegramApiException, IOException;
     }
 
-    public PhotoSize getPhoto(Update update) {
-        // Check that the update contains a message and the message has a photo
-        if (update.hasMessage() && update.getMessage().hasPhoto()) {
-            // When receiving a photo, you usually get different sizes of it
-            List<PhotoSize> photos = update.getMessage().getPhoto();
 
-            // We fetch the bigger photo
-            return photos.stream()
-                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
-                    .findFirst()
-                    .orElse(null);
-        }
-        // Return null if not found
-        return null;
-    }
 
 
     void sendMessageToChannelWithText(String channel, String messageText, String author) throws TelegramApiException {
